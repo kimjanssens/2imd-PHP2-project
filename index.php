1ba3851fb1dd58a043ad0628f5a7f005d0366b60
Kim Janssens <kimjanssens21@gmail.com>
2014-04-11 15:48:23 +0200

    stylesheets gelinkt

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Restuarant web app</title>
+	<link rel="stylesheet" href="css/normalise.css">
+	<link rel="stylesheet" href="css/screen.css">
 </head>
 <body>
 	
