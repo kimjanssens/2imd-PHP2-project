4e6ec52d0034aacc0775d7b516c7530257c734dd
Kim Janssens <kimjanssens21@gmail.com>
2014-04-11 16:51:08 +0200

    login en register form toegevoegd

--- index.php
+++ index.php
@@ -5,10 +5,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>Restuarant web app</title>
-	<link rel="stylesheet" href="css/normalise.css">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/screen.css">
+	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+	<script>
+		function show(){
+			document.getElementById("registerForm").style.display="block";
+		}
+	</script>
 </head>
 <body>
-	
+	<div class="container">
+		<h1>Restaurant app</h1>
+		<h2>Login to continue...</h2>
+		<form action="" method="post">
+			<input type="text" name="name" placeholder="Name">
+			<input type="password" name="password" placeholder="Password">
+			<input type="submit" value="Login" name="btnLogin">
+			<a href="#" onclick="show()">Register</a>
+	</form>
+	<form action="" method="post" id="registerForm">
+		<input type="text" name="nameReg" placeholder="Name">
+		<input type="text" name="street" placeholder="Street">
+		<input type="text" name="city" placeholder="City">
+		<input type="submit" value="Register" name="btnRegister">
+	</form>
+	</div>
 </body>
 </html>
