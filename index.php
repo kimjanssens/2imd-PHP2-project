256f59751847b3d426d40853a25a2f9fca940569
Kim Janssens <kimjanssens21@gmail.com>
2014-04-11 22:40:54 +0200

    register geupdate

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php  
 	include_once("classes/User.class.php");
-	$salt = "#WeAreIMD!";
+	$feedback = "";
 	if (isset($_POST["btnRegister"])) {
 		try {
 			$user = new User();
@@ -8,8 +8,10 @@
 			$user->Street = $_POST["street"];
 			$user->City = $_POST["city"];
 			$user->Phone = $_POST["phone"];
-			$user->Password = md5($_POST["passwordReg"].$salt);
+			$user->Password = $_POST["passwordReg"];
 			$user->Save();
+
+			$feedback = "Thanks for signin up!";
 		} catch (Exception $e) {
 			$error = $e->getMessage();
 		}
@@ -23,14 +25,6 @@
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/screen.css">
 	<script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
-	<script>
-	$(document).ready(function(){
-		$("#registerForm").hide();
-		$("#register").click(function() {
-  			$( "#registerForm" ).slideToggle( "slow" );
-		});
-	});
-	</script>
 </head>
 <body>
 	<div class="container">
@@ -40,22 +34,23 @@
 			<input type="text" name="name" placeholder="Name">
 			<input type="password" name="password" placeholder="Password">
 			<input type="submit" value="Login" name="btnLogin">
-			
-			<a href="#" id="register">Register</a>
-	</form>
-	<form action="" method="post" id="registerForm">
-		<?php  
-			if (isset($error)) {
-				echo "<p class='bg-danger'>$error</p>";
-			}
-		?>
-		<input type="text" name="nameReg" placeholder="Name">
-		<input type="text" name="street" placeholder="Street">
-		<input type="text" name="city" placeholder="City">
-		<input type="tel" name="phone" placeholder="Phonenumber">
-		<input type="password" name="passwordReg" placeholder="Password">
-		<input type="submit" value="Register" name="btnRegister">
-	</form>
+		</form>
+		<form action="" method="post" id="registerForm">
+			<?php  
+				if (isset($error)) {
+					echo "<p class='bg-danger'>$error</p>";
+				}
+				if (!empty($feedback)) {
+					echo "<p class='bg-success'>$feedback</p>";
+				}
+			?>
+			<input type="text" name="nameReg" placeholder="Name" value="<?php if(isset($_POST['nameReg'])){echo $_POST['nameReg'];} ?>">
+			<input type="text" name="street" placeholder="Street" value="<?php if(isset($_POST['street'])){echo $_POST['street'];} ?>">
+			<input type="text" name="city" placeholder="City" value="<?php if(isset($_POST['city'])){echo $_POST['city'];} ?>">
+			<input type="tel" name="phone" placeholder="Phonenumber" value="<?php if(isset($_POST['phone'])){echo $_POST['phone'];} ?>">
+			<input type="password" name="passwordReg" placeholder="Password">
+			<input type="submit" value="Register" name="btnRegister">
+		</form>
 	</div>
 </body>
 </html>
