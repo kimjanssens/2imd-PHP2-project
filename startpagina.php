5f2fd2e5885160897da48240b99ae126e5afd1c8
Kim Janssens <kimjanssens21@gmail.com>
2014-05-07 18:02:54 +0200

    pagina na inloggen met fb

---
+++ startpagina.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<div class="container">
+		<h1>Startpagina</h1>
+		//lijst met restaurants
+	</div>
+</body>
+</html>
