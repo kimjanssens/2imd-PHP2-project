14e2d7dd1cc00f87e67905d4fa9ee91ebfc6d7a0
Kim Janssens <kimjanssens21@gmail.com>
2014-05-12 01:10:29 +0200

    update fb login

--- index.php
+++ index.php
@@ -1,49 +1,31 @@
 <?php
-require 'classes/Facebook.class.php';
-    
-$facebook = new Facebook(array(
-  'appId'  => '1401294226818835',
-  'secret' => 'ef12998a4f3a5af7b65cc06d76657621',
-  'cookie' => true,
-));
- 
-//2. retrieving session
-$session = $facebook->getSession();
- 
-//3. requesting 'me' to API
-$me = null;
-if ($session) {
-  try {
-    $uid = $facebook->getUser();
-    $me = $facebook->api('/me');
-  } catch (FacebookApiException $e) {
-    error_log($e);
-  }
-}
- 
-//4. login or logout
-if ($me) {
-    $logoutUrl = $facebook->getLogoutUrl();
-} else {
-    $loginUrl = $facebook->getLoginUrl();
-}
- 
-?>
-<!doctype html>
-<html xmlns:fb="http://www.facebook.com/2008/fbml">
-  <head>
-    <title>php-sdk</title>
-  </head>
-  <body>
-    <?php if ($me): ?>
-    <?php echo "Welcome, ".$me['first_name']. ".<br />"; ?>
-    <a href="<?php echo $logoutUrl; ?>">
-      <img src="http://static.ak.fbcdn.net/rsrc.php/z2Y31/hash/cxrz4k7j.gif">
-    </a>
-    <?php else: ?>
-      <a href="<?php echo $loginUrl; ?>">
-        <img src="http://static.ak.fbcdn.net/rsrc.php/zB6N8/hash/4li2k73z.gif">
-      </a>
-    <?php endif ?>
-  </body>
+    include_once "Facebook/fbmain.php";
+?><!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<script type="text/javascript" src="http://connect.facebook.net/en_US/all.js"></script>
+    <script type="text/javascript">
+    FB.init({
+        appId  : '<?=$fbconfig['appid']?>',
+        status : true,
+        cookie : true,
+        xfbml  : true
+    });   
+    </script>
+</head>
+<body>
+    <?php if (!$user) { ?>
+        <a href="<?=$loginUrl?>">Facebook Login</a>
+    <?php } else { ?>
+        <a href="<?=$logoutUrl?>">Facebook Logout</a>
+    <?php } ?>
+
+    <?php 
+    if($user){
+    	header("Location: startpagina.php");
+    }
+    ?>
+</body>
 </html>
--- startpagina.php
+++ startpagina.php
@@ -1,19 +1,34 @@
-<!doctype html>
+<?php
+    include_once "Facebook/fbmain.php";
+?><!doctype html>
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
 	<title>Document</title>
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/screen.css">
+    <script type="text/javascript" src="http://connect.facebook.net/en_US/all.js"></script>
+    <script type="text/javascript">
+    FB.init({
+        appId  : '<?=$fbconfig['appid']?>',
+        status : true,
+        cookie : true,
+        xfbml  : true
+    });   
+    </script>
 
 </head>
 <body>
 	<div class="container">
+    <?php 
+    if($user){
+      echo "<img src='".$fqlResult[0]['pic_square']."'>";
+      echo "<a href='".$userInfo['link']."'>".$userInfo['name']."</a>";
+      echo "<br>";
+      echo "<a href='index.php".$logoutUrl."'>Logout</a>";
+    }
+    ?>
 		<h1>Startpagina</h1>
-		<div id="status"></div>
-		<fb:login-button scope="public_profile,email" data-auto-logout-link="true" onlogin="checkLoginState();">
-</fb:login-button>
-		<br>
 		//lijst met restaurants
 	</div>
 </body>
