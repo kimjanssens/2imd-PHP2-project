b54cdbadf39ba14d289fb2078b8f11abff67ff04
Kim Janssens <kimjanssens21@gmail.com>
2014-05-12 16:55:31 +0200

    fb profile toegevoegd

--- tablereservation.php
+++ tablereservation.php
@@ -1,4 +1,5 @@
 <?php
+include_once "Facebook/fbmain.php";
 $restauID = $_GET['id'];
 include_once("classes/Resto.class.php");
 $t = new Resto();
@@ -12,9 +13,19 @@ $t->RestoId = $restauID;
 	<link rel="stylesheet" href="css/bootstrap.min.css">
 	<link rel="stylesheet" href="css/screen.css">
     <link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+
 </head>
 <body>
 <div class="container">
+	<div id="profile">
+      <?php 
+    if($user){
+      echo "<a href='index.php".$logoutUrl."' id='logout'>Logout</a>";
+      echo "<img src='".$fqlResult[0]['pic_square']."'>";
+      echo "<a href='".$userInfo['link']."'>".$userInfo['name']."</a>";
+    }
+    ?>
+    </div>
 	<h2>All the listed tables</h2>
 	<ul id="tables">
 	<?php $t->GetAllTables(); ?>
